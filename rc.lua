adcaa39e11f98e27a91db9119084cb8b58d4eda3
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2021-08-30 20:02:53 +0200

    Mise à jour awesome

--- rc.lua
+++ rc.lua
@@ -198,7 +198,6 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
         },
     }
 end)
